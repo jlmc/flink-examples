6b4f0e0021311d56905381a8fab855ceda1a8e3c
Joao Costa <costajlmpp@gmail.com>
2026-01-19 02:12:14 +0000

    Set parallelism to 1 and enable checkpointing for Kafka source connector

--- projects/flink-data-sources/data-source-kafka-connector/src/main/java/io/github/jlmc/j8/KafkaSourceConnectorStringValueOnly.java
+++ projects/flink-data-sources/data-source-kafka-connector/src/main/java/io/github/jlmc/j8/KafkaSourceConnectorStringValueOnly.java
@@ -17,17 +17,24 @@ import org.slf4j.LoggerFactory;
 public class KafkaSourceConnectorStringValueOnly {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaSourceConnectorStringValueOnly.class);
-    
+
     public static void main(String[] args) throws Exception {
 
         Configuration conf = new Configuration();
         // Set the web UI port to 8081 (or another free port) to avoid conflicts if multiple jobs are running
         conf.set(RestOptions.PORT, 8081);
         conf.set(TaskManagerOptions.NUM_TASK_SLOTS, 4);
+        //org.apache.flink.configuration.PipelineOptions.conf.set(org.apache.flink.configuration.CoreOptions.DEFAULT_PARALLELISM, 2);
+        // Ensure that checkpoints can be triggered even if some tasks have finished
+        // This is useful for jobs that might finish quickly in a local environment
+       // org.apache.flink.configuration.PipelineOptions.conf.set(org.apache.flink.configuration.ConfigOptions.key("execution.checkpointing.checkpoints-after-tasks-finish.enabled").booleanType().defaultValue(true), true);
 
         @SuppressWarnings("resource")
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
         LOGGER.info("Hello, Kafka Source Connector with String Value Only!");
+
+
+        env.setParallelism(1); // Set parallelism to 1 for simplicity, by default it is the number of CPU cores
 
         env.enableCheckpointing(3000, CheckpointingMode.EXACTLY_ONCE);
         KafkaSource<String> kafkaSource =
@@ -37,12 +44,11 @@ public class KafkaSourceConnectorStringValueOnly {
                         .setGroupId("KafkaSourceConnectorStringValueOnly")
                         // configure other options as needed
                         .setStartingOffsets(OffsetsInitializer.committedOffsets(OffsetResetStrategy.EARLIEST))
+                        //.setBounded(org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer.latest())
                         .setValueOnlyDeserializer(new SimpleStringSchema())
                         .setProperty(KafkaSourceOptions.COMMIT_OFFSETS_ON_CHECKPOINT.key(), "true")
-                        .setProperty(org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "5000")
+                        //.setProperty(org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "5000")
                         .build();
-
-
 
         env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source")
                 .name("Kafka Source")

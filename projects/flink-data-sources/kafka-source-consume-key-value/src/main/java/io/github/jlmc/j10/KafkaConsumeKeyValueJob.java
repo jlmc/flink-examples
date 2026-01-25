b33c647948f14401ec450f6b96c0bff30f64a7f5
Joao Costa <costajlmpp@gmail.com>
2026-01-25 18:05:05 +0000

    feat(kafka-source): update Kafka consumer to handle key-value pairs with custom deserialization

--- projects/flink-data-sources/kafka-source-consume-key-value/src/main/java/io/github/jlmc/j10/KafkaConsumeKeyValueJob.java
+++ projects/flink-data-sources/kafka-source-consume-key-value/src/main/java/io/github/jlmc/j10/KafkaConsumeKeyValueJob.java
@@ -1,19 +1,33 @@
 package io.github.jlmc.j10;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.configuration.RestOptions;
 import org.apache.flink.configuration.TaskManagerOptions;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.KafkaSourceOptions;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.connector.kafka.source.reader.deserializer.KafkaRecordDeserializationSchema;
 import org.apache.flink.core.execution.CheckpointingMode;
 import org.apache.flink.formats.json.JsonDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+import org.apache.flink.util.jackson.JacksonMapperFactory;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 
 /**
  * kafka-source-json-consumer
@@ -31,7 +45,7 @@ public class KafkaConsumeKeyValueJob {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(conf);
 
-        DataStream<PersonLocationEvent> stream = createJob(env, "localhost:9092");
+        DataStream<Tuple2<String, PersonLocationEvent>> stream = createJob(env, "localhost:9092");
 
         stream.print()
               .name("Print to Console")
@@ -40,7 +54,7 @@ public class KafkaConsumeKeyValueJob {
         env.execute("Kafka Source Connector with String Value Only");
     }
 
-    public static DataStream<PersonLocationEvent> createJob(StreamExecutionEnvironment env, String bootstrapServers) {
+    public static DataStream<Tuple2<String, PersonLocationEvent>> createJob(StreamExecutionEnvironment env, String bootstrapServers) {
         LOGGER.info("Hello, Kafka Source Connector with String Value Only!");
 
         env.setParallelism(1); // Set parallelism to 1 for simplicity, by default it is the number of CPU cores
@@ -49,8 +63,8 @@ public class KafkaConsumeKeyValueJob {
 
         JsonDeserializationSchema<PersonLocationEvent> deserializationSchema = new JsonDeserializationSchema<>(PersonLocationEvent.class);
 
-        KafkaSource<PersonLocationEvent> kafkaSource =
-                KafkaSource.<PersonLocationEvent>builder()
+        KafkaSource<Tuple2<String, PersonLocationEvent>> kafkaSource =
+                KafkaSource.<Tuple2<String, PersonLocationEvent>>builder()
                         .setBootstrapServers(bootstrapServers)
                         .setTopics("person-location-events")
                         .setGroupId("KafkaSourceJsonConsumerJob")
@@ -58,7 +72,38 @@ public class KafkaConsumeKeyValueJob {
                         .setStartingOffsets(OffsetsInitializer.committedOffsets(OffsetResetStrategy.EARLIEST))
                         //.setBounded(org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer.latest())
 
-                        .setValueOnlyDeserializer(deserializationSchema)
+                        //.setValueOnlyDeserializer(deserializationSchema)
+                        .setDeserializer(new KafkaRecordDeserializationSchema<Tuple2<String, PersonLocationEvent>>() {
+                            @Override
+                            public TypeInformation<Tuple2<String, PersonLocationEvent>> getProducedType() {
+                                return new TupleTypeInfo<>(Types.TUPLE(Types.STRING, Types.POJO(PersonLocationEvent.class)));
+                            }
+
+                            @Override
+                            public void deserialize(ConsumerRecord<byte[], byte[]> consumerRecord, Collector<Tuple2<String, PersonLocationEvent>> collector) throws IOException {
+
+                                // when the key is a Interger we can use ByteBuffer to convert byte[] to int
+                                //int key = ByteBuffer.wrap(consumerRecord.key()).getInt();
+                                // Deserialize the key (assuming UTF-8 encoding)
+
+                                String key = null;
+                                if (consumerRecord.key() != null) {
+                                    key = new String(consumerRecord.key(), StandardCharsets.UTF_8);
+                                }
+
+                                // Deserialize the value (using JSON as an example)
+                                PersonLocationEvent value = null;
+                                if (consumerRecord.value() != null) {
+                                    ObjectMapper mapper = new ObjectMapper();
+                                    value = mapper.readValue(consumerRecord.value(), PersonLocationEvent.class);
+                                }
+
+                                // Emit the tuple
+                                collector.collect(Tuple2.of(key, value));
+
+                            }
+                        })
+
 
                         .setProperty(KafkaSourceOptions.COMMIT_OFFSETS_ON_CHECKPOINT.key(), "true")
                         //.setProperty(org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "5000")
@@ -66,6 +111,7 @@ public class KafkaConsumeKeyValueJob {
 
         return env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source")
                 .name("Kafka Source")
-                .uid("kafka-source");
+                .uid("kafka-source")
+                ;
     }
 }

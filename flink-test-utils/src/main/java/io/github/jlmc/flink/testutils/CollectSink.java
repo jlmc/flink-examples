d65b5e540afea258013a512e112d7c2424f08aa7
Joao Costa <costajlmpp@gmail.com>
2026-02-21 00:08:01 +0000

    feat(tests): add JUnit 5 extension for Kafka support using Testcontainers

---
+++ flink-test-utils/src/main/java/io/github/jlmc/flink/testutils/CollectSink.java
@@ -0,0 +1,30 @@
+package io.github.jlmc.flink.testutils;
+
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * A simple sink that collects elements into a static list.
+ * Note: Since this is usually used in a MiniCluster, the static list works as long as
+ * the tests run in the same JVM.
+ */
+public class CollectSink<T> implements SinkFunction<T> {
+    public static final List<Object> VALUES = Collections.synchronizedList(new ArrayList<>());
+
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> values() {
+        return (List<T>) VALUES;
+    }
+
+    @Override
+    public void invoke(T value, Context context) {
+        VALUES.add(value);
+    }
+
+    public static void clear() {
+        VALUES.clear();
+    }
+}
---
+++ flink-test-utils/src/main/java/io/github/jlmc/flink/testutils/kafka/EnableKafka.java
@@ -0,0 +1,34 @@
+package io.github.jlmc.flink.testutils.kafka;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Meta-annotation that enables Kafka support for JUnit 5 tests.
+ * This annotation uses {@link KafkaExtension} to manage the lifecycle of a Kafka container
+ * using Testcontainers.
+ *
+ * <p>Example usage:
+ * <pre>{@code
+ * @EnableKafka
+ * class MyKafkaIT {
+ *     @Test
+ *     void testWithKafka() {
+ *         String brokers = KafkaExtension.getBootstrapServers();
+ *         // or use the "brokers" system property
+ *         String brokersProp = System.getProperty("brokers");
+ *
+ *         // setup Kafka producer/consumer and run tests
+ *     }
+ * }
+ * }</pre>
+ */
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+@ExtendWith(KafkaExtension.class)
+public @interface EnableKafka {
+}
